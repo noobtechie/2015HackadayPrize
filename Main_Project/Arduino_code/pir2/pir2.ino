30e448c8ff1ff6033397a339719ac93538aa1e81
Sounak Ranjan Das <sounakranjandas9@gmail.com>
2015-07-16 17:45:17 +0530

    Added Comments

--- Main_Project/Arduino_code/pir2/pir2.ino
+++ Main_Project/Arduino_code/pir2/pir2.ino
@@ -1,14 +1,20 @@
+//Include files
+
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 #include "LowPower.h"
 
+//Defines for network
+
 #define PIR 4
 #define NET_ID 1
 #define NODE_ID 2
 
 int pirval;
+
+//Structure to packetize data
 
 typedef struct{
   uint8_t from;
@@ -20,14 +26,17 @@ typedef struct{
 
 payload pir2;
 
-RF24 radio(9,10);
+RF24 radio(9,10); //Initialize radio class
 
-const uint64_t pipe = 0xDEADBEEF02;
+const uint64_t pipe = 0xDEADBEEF02; //Pipe address for this node
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
   printf_begin();
+  
+  //Radio settings
+  
   radio.begin();
   radio.setRetries(15,15);
   radio.setDataRate(RF24_250KBPS);
@@ -42,13 +51,19 @@ void setup() {
 }
 
 void loop() {
+  
+  //Power up radio from sleep
+  
   radio.powerUp();
-  // put your main code here, to run repeatedly:
+  // Save sensor data in structure
+  
   pirval = digitalRead(PIR);
   pir2.from = NODE_ID;
   pir2.to = 0;
   pir2.type = 2;
   pir2.data1 = pirval;
+  
+  //Wait till data is transmitted
   
   bool ok = false;
   while(!ok)
@@ -57,6 +72,8 @@ void loop() {
   printf("ok\n");
   //else
   //printf("failed\n");
+  //Power down radio and node
+  
   radio.powerDown();
   LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF); 
   LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
