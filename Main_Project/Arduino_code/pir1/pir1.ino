e0d161c5c4dda106b228153ef713c218e079f83d
Sounak Ranjan Das <sounakranjandas9@gmail.com>
2015-07-16 17:41:06 +0530

    Added Comments

--- Main_Project/Arduino_code/pir1/pir1.ino
+++ Main_Project/Arduino_code/pir1/pir1.ino
@@ -1,14 +1,20 @@
+//Include files
+
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 #include "LowPower.h"
 
+//Defines for network
+
 #define PIR 4
 #define NET_ID 1
 #define NODE_ID 1
 
 int pirval;
+
+//Structure to packetize data
 
 typedef struct{
   uint8_t from;
@@ -20,7 +26,7 @@ typedef struct{
 
 payload pir1;
 
-RF24 radio(9,10);
+RF24 radio(9,10); //Initialize radio class
 
 const uint64_t pipe = 0xDEADBEEF01;
 
@@ -28,6 +34,9 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
   printf_begin();
+  
+  //Radio settings
+  
   radio.begin();
   radio.setRetries(15,15);
   radio.setDataRate(RF24_250KBPS);
@@ -42,19 +51,27 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Power up radio
   radio.powerUp();
+  
+  //Save sensor data to structure
+  
   pirval = digitalRead(PIR);
   pir1.from = NODE_ID;
   pir1.to = 0;
   pir1.type = 2;
   pir1.data1 = pirval;
   
+  //Wait till data is transmitted
+  
   bool ok = false;
   while(!ok)
   ok = radio.write(&pir1,sizeof(payload));
   //if(ok)
   printf("ok\n");
+  
+  //Power down radio and node
+  
   radio.powerDown();
   LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF); 
   LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
